35a88d05f4156c2d83b1293a99345c05ab0e5f55
AbandonT <baochireai0831>
2021-01-20 11:59:40 +0800

    发布

---
+++ src/main/java/com/usian/pojo/User.java
@@ -0,0 +1,18 @@
+package com.usian.pojo;
+
+/**
+ * @author Abandon
+ * @description User
+ * @date 2021/1/20 11:57
+ */
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
